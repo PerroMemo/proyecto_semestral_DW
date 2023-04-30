42b2d9c20c594d4e9e5443a5d6897adad5838c72
CharlesEspina <127886553+CharlesEspina@users.noreply.github.com>
2023-04-30 18:46:30 -0400

    Add files via upload
    Actualizacion a JQuery

--- script/script.js
+++ script/script.js
@@ -34,3 +34,74 @@ function quitarBordes(){
         items[i].style.border = "none";
     }
 }
+
+$(document).ready(function() {
+    $("#miFormulario").submit(function(event) {
+      event.preventDefault();
+
+      let primerNombre = $("#primerNombre").val();
+      let segundoNombre = $("#segundoNombre").val();
+      let email = $("#email").val();
+      let telefono = $("#telefono").val();
+      let contrasena = $("#contrasena").val();
+
+      
+      if (primerNombre === "" || email === "" || telefono === "" || contrasena === "") {
+        alert("Por favor, rellena todos los campos.");
+        return false;
+      }
+
+      
+      let emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegEx.test(email)) {
+        alert("Por favor, introduce una dirección de correo electrónico válida.");
+        return false;
+      }
+
+      
+      let telefonoRegEx = /^\d{10}$/;
+      if (!telefonoRegEx.test(telefono)) {
+        alert("Por favor, introduce un número telefónico válido (10 dígitos).");
+        return false;
+      }
+
+      
+      if (contrasena.length < 8) {
+        alert("Por favor, introduce una contraseña con al menos 8 caracteres.");
+        return false;
+      }
+
+      $("#miFormulario").unbind("submit").submit();
+    });
+});
+
+$(document).ready(function() {
+
+    var loginForm = $('#login-form');
+    var emailInput = $('#email');
+    var passwordInput = $('#password');
+  
+
+    function validateEmail(email) {
+      var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return re.test(email);
+    }
+  
+
+    loginForm.submit(function(event) {
+
+      if (!validateEmail(emailInput.val())) {
+        alert('Por favor ingrese un correo electrónico válido');
+        event.preventDefault();
+        return;
+      }
+  
+
+      if (passwordInput.val().length < 6) {
+        alert('Por favor ingrese una contraseña válida (mínimo 6 caracteres)');
+        event.preventDefault();
+        return;
+      }
+    });
+  });
+  
